52ff3488e717df68c5ca28546fd97e4ed4ce9bca
jmadoremos <21135564+jmadoremos@users.noreply.github.com>
2024-05-23 09:23:04 +0000

    Rearrange properties and add URIs

--- src/GraphQL/Repositories/StarWars/Planets/PlanetApiResponse.cs
+++ src/GraphQL/Repositories/StarWars/Planets/PlanetApiResponse.cs
@@ -6,22 +6,26 @@ public class PlanetApiResponse : SwapiResponse
 {
     public string Name { get; set; } = default!;
 
+    public string Diameter { get; set; } = default!;
+
     [JsonProperty("rotation_period")]
     public string RotationPeriod { get; set; } = default!;
 
     [JsonProperty("orbital_period")]
     public string OrbitalPeriod { get; set; } = default!;
 
-    public string Diameter { get; set; } = default!;
+    public string Gravity { get; set; } = default!;
+
+    public string Population { get; set; } = default!;
 
     public string Climate { get; set; } = default!;
-
-    public string Gravity { get; set; } = default!;
 
     public string Terrain { get; set; } = default!;
 
     [JsonProperty("surface_water")]
     public string SurfaceWater { get; set; } = default!;
 
-    public string Population { get; set; } = default!;
+    public IEnumerable<Uri> Residents { get; set; } = default!;
+
+    public IEnumerable<Uri> Films { get; set; } = default!;
 }
